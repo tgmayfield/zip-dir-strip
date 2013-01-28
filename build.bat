3a9ce83cd483d81a85a3eafbbb2adf6564bb13c1
Thomas G Mayfield <tgmayfield@gmail.com>
2013-01-28 13:30:22 -0700

    Allow passing build arguments to MSBuild

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 @echo off
 packages\NuGet.exe install ZipStrip\packages.config -o .\packages
 packages\NuGet.exe install ZipDirStrip\packages.config -o .\packages
-msbuild ZipDirStrip.sln
+msbuild ZipDirStrip.sln %*
